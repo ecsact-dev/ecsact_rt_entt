5b8e0691938863baf1365fe35936fc6af075624f
Ezekiel Warren <zekewarren@gmail.com>
2022-06-28 09:40:05 -0700

    tabs

--- runtime/meta.template.cc
+++ runtime/meta.template.cc
@@ -8,12 +8,12 @@ size_t ecsact_meta_component_size
 	( ecsact_component_id comp_id
 	)
 {
-  return runtime.component_size(static_cast<ecsact::component_id>(comp_id));
+	return runtime.component_size(static_cast<ecsact::component_id>(comp_id));
 }
 
 size_t ecsact_meta_action_size
 	( ecsact_system_id action_id
 	)
 {
-  return runtime.action_size(static_cast<ecsact::action_id>(action_id));
+	return runtime.action_size(static_cast<ecsact::action_id>(action_id));
 }
