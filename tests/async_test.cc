07f458cb1f95c8d5c4a9ea2876027d3e22b57312
Kelwan <austin@kelway.ca>
2022-11-21 15:07:55 -0500

    adds testable async module

---
+++ tests/async_test.cc
@@ -0,0 +1,13 @@
+#include "async_test.ecsact.systems.hh"
+
+void async_test::AddComponent::impl(context& ctx) {
+}
+
+void async_test::UpdateComponent::impl(context& ctx) {
+}
+
+void async_test::Blah::impl(context& ctx) {
+}
+
+void async_test::RemoveComponent::impl(context& ctx) {
+}
