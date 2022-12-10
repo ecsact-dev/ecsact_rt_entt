0cb651e684f2644ec441a5356fcedd63f1defa8e
Ezekiel Warren <zekewarren@gmail.com>
2022-12-10 13:41:41 -0800

    formatting

--- runtime/test/debug_print_entt_views.cc
+++ runtime/test/debug_print_entt_views.cc
@@ -4,7 +4,11 @@
 #include "ecsact/entt/system_view.hh"
 #include "runtime_test.ecsact.meta.hh"
 
-auto replace_all(std::string& str, std::string_view find, std::string_view replace) {
+auto replace_all(
+	std::string&     str,
+	std::string_view find,
+	std::string_view replace
+) {
 	auto idx = str.find(find);
 	while(idx != std::string::npos) {
 		str.replace(idx, find.size(), replace);
@@ -31,20 +35,18 @@ auto pretty_type_string() {
 template<typename S>
 auto print_system_views() {
 	using boost::mp11::mp_for_each;
-	using ecsact::entt::system_view_type;
 	using ecsact::entt::system_association_views_type;
+	using ecsact::entt::system_view_type;
 
 	std::cout //
 		<< "  " << pretty_type_string<S>() << "\n"
 		<< "  | " << pretty_type_string<system_view_type<S>>() << "\n";
 
-	mp_for_each<system_association_views_type<S>>(
-		[]<typename V>(V) {
+	mp_for_each<system_association_views_type<S>>([]<typename V>(V) {
 		std::cout //
 			<< "  | " << pretty_type_string<V>() << " (association view)\n";
-		}
-	);
-	
+	});
+
 	std::cout << "\n";
 }
 
