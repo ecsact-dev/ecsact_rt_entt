65f5fb5571e2f90db5133315cb5373cdae949f62
Ezekiel Warren <zekewarren@gmail.com>
2023-04-07 15:04:48 -0700

    fix: handling recursive imported packages correctly

--- ecsact/entt/detail/meta_util.hh
+++ ecsact/entt/detail/meta_util.hh
@@ -1,21 +1,61 @@
 #pragma once
 
-#include <map>
-#include <boost/mp11.hpp>
 #include "ecsact/runtime/common.h"
+#include "ecsact/lib.hh"
+#include <boost/mp11.hpp>
+#include <map>
 
 namespace ecsact::entt::detail {
 
+template<typename... Package>
+struct mp_components_t;
+
+template<>
+struct mp_components_t<> {
+	using type = ::ecsact::mp_list<>;
+};
+
+template<typename Package>
+struct mp_components_t<Package> {
+	using type = typename Package::components;
+};
+
+template<typename HeadPackage, typename... Package>
+struct mp_components_t<HeadPackage, Package...> {
+	using type =
+		boost::mp11::mp_unique<boost::mp11::mp_flatten<boost::mp11::mp_push_back<
+			typename mp_components_t<HeadPackage>::type,
+			typename mp_components_t<Package...>::type>>>;
+};
+
+template<typename Package>
+using mp_package_dependencies = typename Package::dependencies;
+
+template<typename PackageList>
+using mp_package_dependencies_from_list = std::enable_if_t<
+	!boost::mp11::mp_empty<PackageList>::value,
+	boost::mp11::mp_flatten<
+		boost::mp11::mp_transform<mp_package_dependencies, PackageList>>>;
+
+template<typename PackageList>
+using mp_package_dependencies_recursive =
+	boost::mp11::mp_unique<boost::mp11::mp_apply<
+		boost::mp11::mp_append,
+		boost::mp11::mp_iterate<
+			PackageList,
+			boost::mp11::mp_identity_t,
+			mp_package_dependencies_from_list>>>;
+
 template<typename Package, typename Callback>
 void mp_for_each_available_component(Callback&& cb) {
+	using boost::mp11::mp_append;
 	using boost::mp11::mp_for_each;
-	using components = typename Package::components;
-	using dependencies = typename Package::dependencies;
+
+	using components = typename mp_append<
+		mp_components_t<Package>,
+		mp_package_dependencies_recursive<typename Package::dependencies>>::type;
 
 	mp_for_each<components>([&]<typename C>(const C& c) { cb(c); });
-	mp_for_each<dependencies>([&]<typename D>(const D& d) {
-		mp_for_each_available_component<D>(cb);
-	});
 }
 
 template<typename Package, typename Callback>
