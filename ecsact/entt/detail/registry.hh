3e14084f25ab234d154a3272688555bebf63cfaa
Kelwan <austin@kelway.ca>
2024-06-07 14:08:12 -0700

    chore: remove static allocator

--- ecsact/entt/detail/registry.hh
+++ ecsact/entt/detail/registry.hh
@@ -1,10 +1,8 @@
 #pragma once
 
 #include "entt/entity/registry.hpp"
-#include "ecsact/entt/detail/allocator.hh"
 
 namespace ecsact::entt {
 
-using registry_t = ::entt::
-	basic_registry<::entt::entity, detail::static_allocator<::entt::entity>>;
+using registry_t = ::entt::basic_registry<::entt::entity>;
 }
