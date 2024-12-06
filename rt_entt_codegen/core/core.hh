887b2e21802cd7dcfa83df325b985cd4342572ea
Ezekiel <ezekiel@seaube.com>
2024-12-06 15:48:04 -0800

    feat: implement new hash registry method

--- rt_entt_codegen/core/core.hh
+++ rt_entt_codegen/core/core.hh
@@ -96,4 +96,9 @@ auto print_copy_components(
 	const ecsact_entt_details& details
 ) -> void;
 
+auto print_hash_registry(
+	codegen_plugin_context&    ctx,
+	const ecsact_entt_details& details
+) -> void;
+
 } // namespace ecsact::rt_entt_codegen::core
